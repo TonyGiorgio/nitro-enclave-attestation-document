1e513ae6a9edb6ebbd821b54eb418cef6fd2d7f8
Tony Giorgio <tonygiorgio@protonmail.com>
2024-09-24 13:11:58 -0500

    Add webassembly

--- flake.nix
+++ flake.nix
@@ -1,13 +1,14 @@
 {
-  description = "Rust project";
+  description = "Minimal rust wasm32-unknown-unknown example";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+      url = "github:oxalica/rust-overlay/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
@@ -15,35 +16,32 @@
       let
         overlays = [ rust-overlay.overlays.default ];
         pkgs = import nixpkgs { inherit system overlays; };
-        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         inputs = [
-            rust
-            pkgs.rust-analyzer
-            pkgs.pkg-config
-            pkgs.openssl
-            pkgs.zlib
-            pkgs.gcc
-            pkgs.clang
-            pkgs.jq
-            pkgs.runc
-            pkgs.conmon
-            pkgs.slirp4netns
-            pkgs.fuse-overlayfs
-        ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.libiconv
+          rustToolchain
+          pkgs.rust-analyzer
+          pkgs.openssl
+          pkgs.zlib
+          pkgs.gcc
+          pkgs.pkg-config
+          pkgs.just
+          pkgs.wasm-pack
+          pkgs.wasm-bindgen-cli
+          pkgs.binaryen
+          pkgs.clang
+        ] ++ pkgs.lib.optionals (!pkgs.stdenv.isDarwin) [
+          pkgs.firefox
+          pkgs.geckodriver
         ];
       in
       {
         defaultPackage = pkgs.rustPlatform.buildRustPackage {
           src = ./.;
-
           cargoLock = {
             lockFile = ./Cargo.lock;
           };
-
           nativeBuildInputs = inputs;
         };
-
 
         devShell = pkgs.mkShell {
           packages = inputs;
@@ -52,9 +50,6 @@
             export LD_LIBRARY_PATH=${pkgs.openssl}/lib:$LD_LIBRARY_PATH
             export CC_wasm32_unknown_unknown=${pkgs.llvmPackages_14.clang-unwrapped}/bin/clang-14
             export CFLAGS_wasm32_unknown_unknown="-I ${pkgs.llvmPackages_14.libclang.lib}/lib/clang/14.0.6/include/"
-            export PKG_CONFIG_PATH=${pkgs.openssl.dev}/lib/pkgconfig
-
-            export RUST_LOG=info
           '';
         };
       }
